621adbfae6e3db8e28e3d245d198de7b5bf8e1e8
Zhiyang Liu <33142752+zhyangl@users.noreply.github.com>
2019-03-15 18:40:45 -0400

    [add] Valid Palindrome II (skip a char)

---
+++ ValidPalindromeII.java
@@ -0,0 +1,29 @@
+class ValidPalindromeII {
+    public boolean validPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        int left = 0;
+        int right = s.length() - 1;
+        
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return validSubstring(s, left + 1, right) || validSubstring(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    
+    private boolean validSubstring(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
